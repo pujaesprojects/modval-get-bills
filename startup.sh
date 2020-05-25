42d94420beab89d2ff569a9d351bda323d1e928c
jandelahoz <jandelahoz@gmail.com>
2020-05-25 17:47:05 -0500

    Initial commit

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build . -t pujaesprojects/modval-get-bills:1.0
-docker run -d -p 8990:8080 --name get-bills-01 pujaesprojects/modval-get-bills:1.0
+docker run -d -p 8990:8080 --name modval-get-bills pujaesprojects/modval-get-bills:1.0
